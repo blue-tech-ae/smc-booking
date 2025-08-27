fa7de7d24d70d34e28e0a786b146352146ed307f
blue-tech-ae <programingbluetech@gmail.com>
2025-08-27 09:50:31 +0300

    Add campus and location booking APIs

--- app/Http/Controllers/Api/LocationController.php
+++ app/Http/Controllers/Api/LocationController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Location;
+use App\Models\Event;
+use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 /**
  * @OA\Get(
@@ -33,5 +36,42 @@ class LocationController extends Controller
         $locations = Location::select('id', 'name', 'description', 'campus')->get();
 
         return response()->json(['data' => $locations]);
+    }
+
+    /**
+     * Return campuses with their locations
+     */
+    public function campuses(): JsonResponse
+    {
+        $campuses = Location::select('id', 'name', 'description', 'campus')
+            ->get()
+            ->groupBy('campus')
+            ->map(fn ($locations, $campus) => [
+                'campus' => $campus,
+                'locations' => $locations->map(fn ($location) => $location->only(['id', 'name', 'description']))->values(),
+            ])
+            ->values();
+
+        return response()->json(['data' => $campuses]);
+    }
+
+    /**
+     * Return booked days for a location in a given month
+     */
+    public function bookedDays(Location $location, Request $request): JsonResponse
+    {
+        $month = $request->query('month');
+
+        $start = Carbon::parse($month . '-01')->startOfMonth();
+        $end = Carbon::parse($month . '-01')->endOfMonth();
+
+        $days = Event::where('location_id', $location->id)
+            ->whereBetween('start_time', [$start, $end])
+            ->pluck('start_time')
+            ->map(fn ($date) => Carbon::parse($date)->day)
+            ->unique()
+            ->values();
+
+        return response()->json(['data' => $days]);
     }
 }
